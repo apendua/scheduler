2c992e492a287d1ab71f8f08b1c886078abc7813
apendua <apendua@gmail.com>
2014-06-02 16:28:20 +0200

    Added .travis.yml and a dummy test

---
+++ tests/specs/benchmark.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+
+describe('Becnhmark test suite.', function () {
+  "use strict";
+  it('should be able to perform a simple assertion.', function () {
+    expect(true).to.be.true;
+  });
+});
