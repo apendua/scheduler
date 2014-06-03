509e7ccfc506ec17f4bcc07d482f66b3456ac0ed
apendua <apendua@gmail.com>
2014-06-03 03:13:42 +0200

    Added another test suite

---
+++ tests/specs/scheduler.js
@@ -0,0 +1,44 @@
+var expect = require('chai').expect;
+var promise = require('../helpers').promise;
+var throttle = require('../helpers').throttle;
+
+describe('Scheduler.', function () {
+  var server, client;
+  
+  setup(function (_server, _client) {
+    server = _server; client = _client;
+  });
+  
+  before(function (done) {
+    promise(server)
+      .eval(function () {
+        Scheduler.job('testJob', function () {
+          emit('testJob');
+        });
+      })
+      .always(done);
+  });
+  
+  it('should be able to trigger an existing job.', function (done) {
+    // TODO: test the time difference
+    
+    var timestamp = Date.now();
+    
+    done = throttle(done).times(2);
+    
+    promise(client)
+      .eval(function () {
+        Scheduler.addEvent('testJob', moment().add('seconds', 3).toISOString());
+      })
+      .always(done);
+    
+    promise(server)
+      .once('testJob', function () {
+        return Date.now() - timestamp;
+      })
+      .then(function (value) {
+        expect(value).to.be.ok;
+      })
+      .always(done);
+  });
+});
