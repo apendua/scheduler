fa5cb90bf0997dd166d5bf5afd63e915fdd25c16
apendua <apendua@gmail.com>
2014-06-07 00:22:10 +0200

    Added loginRequired template

--- client/routes.js
+++ client/routes.js
@@ -1,32 +1,51 @@
+MyController = RouteController.extend({
+  action: function () {
+    if (Meteor.user()) {
+      this.render();
+    } else {
+      this.render('loginRequired');
+    }
+  },
+});
+
 Router.map(function () {
   "use strict";
   
   // CLIENT ROUTES
   this.route('home', {
-    path   : '/',
-    where  : 'client',
-    waitOn : function () {
-      return [
-        Meteor.subscribe('apiKeys'),
-        Meteor.subscribe('users'),
-        Meteor.subscribe('staffMembers'),
-      ];
-    }
+    controller : MyController,
+    path       : '/',
+    where      : 'client',
+    waitOn     : function () {
+      if (Meteor.user()) {
+        return [
+          Meteor.subscribe('apiKeys'),
+          Meteor.subscribe('users'),
+          Meteor.subscribe('staffMembers'),
+        ];
+      }
+    },
   });
   
   this.route('listOfJobs', {
-    path   : '/listOfJobs',
-    where  : 'client',
-    waitOn : function () {
-      return Meteor.subscribe('activeJobs');
+    controller : MyController,
+    path       : '/listOfJobs',
+    where      : 'client',
+    waitOn     : function () {
+      if (Meteor.user()) {
+        return Meteor.subscribe('activeJobs');
+      }
     }
   });
 
   this.route('history', {
-    path   : '/history',
-    where  : 'client',
-    waitOn : function () {
-      return Meteor.subscribe('history');
+    controller : MyController,
+    path       : '/history',
+    where      : 'client',
+    waitOn     : function () {
+      if (Meteor.user()) {
+        return Meteor.subscribe('history');
+      }
     }
   });
 
